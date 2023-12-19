edc958d0c5ee5e11430980356ec6f4b3fe55cb85
Danilo <daniloskype459@gmail.com>
2023-12-18 23:36:14 -0300

    add hasRole middleware

---
+++ src/middlewares/hasRole.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+
+export function hasRole(role: string) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        const user = req.user;
+        if (user?.role === role) {
+            next();
+        } else {
+            res.sendStatus(403);
+        }
+    };
+}
